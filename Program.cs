685b23c5af49fe004b452b3fa940bb21b31f29a6
Powell, Tyler <tspowell@tampabay.rr.com>
2019-08-29 15:55:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloISM4300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
